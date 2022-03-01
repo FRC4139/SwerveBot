4021d37faba66826c5227ad79a57cf08fc126fe2
Kevin1729 <kxia1729@gmail.com>
2022-02-28 20:52:02 -0800

    Removed direct (Euclidean) distance measures, updated methods to account for camera pitch and yaw

--- src/main/java/frc/robot/Parallax.java
+++ src/main/java/frc/robot/Parallax.java
@@ -5,6 +5,7 @@ public class Parallax {
     public static double TAPE_HEIGHT = 8.0; //feet
     public static double HEIGHT_DIFF = TAPE_HEIGHT - LIMELIGHT_HEIGHT; //feet
     public static double DEFAULT_THRESHOLD = 0.1; //feet
+    public static double CAMERA_PITCH = 0.0; //radians
     /**
      * Returns horizontal distance from limelight to basket using parallax. Requires tx and ty from two viewpoints.
      * @param dx Change in translational distance
@@ -17,24 +18,21 @@ public class Parallax {
     public static double getDistanceToTarget(double tx1, double ty1, double tx2, double ty2, double dx, double dz){
         //Returns horizontal distance from lightlight to basket using parallax
         double distance;
+        double correctedTy1, correctedTy2;
+        
+        correctedTy1 = ty1 + CAMERA_PITCH;
+        correctedTy2 = ty2 + CAMERA_PITCH;
 
-
-        double k = dz / (Math.tan(ty2) / Math.tan(ty1) - 1);
+        double k = dz / (Math.tan(correctedTy2) / Math.tan(correctedTy1) - 1);
         distance = k / Math.cos(tx2);
         return distance;
     }
-    public static double getDistanceToTarget(double tx1, double ty1, double tx2, double ty2, double dx, double dz, boolean getDirectDistance){
-        //if getDirectDistance is true, returns Euclidean distance using parallax
-        double distance;
-        if(getDirectDistance){
-            double k = dz / (Math.tan(ty2) / Math.tan(ty1) - 1);
-            distance = k * Math.sqrt(Math.pow(Math.tan(tx2),2) + Math.pow(Math.tan(ty2),2) + 1);
-            return distance;
-        }
-        else{
-            return getDistanceToTarget(tx1, ty1, tx2, ty2, dx, dz);
-        }
+
+    public static double getDistanceToTarget(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double yaw){
+        double correctedTx = tx2 + yaw;
+        return getDistanceToTarget(tx1, ty1, correctedTx, ty2, dx, dz);
     }
+    
     /**
      * Returns the horizontal distance from limelight to basket using known heights of limelight and basket.
      * Formula: HEIGHT_DIFF / (tan(ty) * cos(tx))
@@ -45,38 +43,19 @@ public class Parallax {
     public static double getDistanceToTarget(double tx, double ty){
         //Returns horizontal distance from limelight to basket using known height of basket
         double distance;
-        distance = HEIGHT_DIFF / Math.tan(ty) / Math.cos(tx);
+        double correctedTy = ty + CAMERA_PITCH;
+        distance = HEIGHT_DIFF / Math.tan(correctedTy) / Math.cos(tx);
         return distance;
     }
-    /**
-     * Returns the distance from limelight to basket, either horizontal or straight-line, depending on getDirectDistance.
-     * @param tx Horizontal offset from crosshair to target
-     * @param ty Vertical offset from crosshair to target
-     * @param getDirectDistance If true, calculate the Euclidean distance (Straight-line distance, vertical included). If false, calculate horizontal distance (no vertical).
-     * Formula for Euclidean distance: (HEIGHT_DIFF / tan(ty)) * norm([tan(tx),tan(ty),1])
-     * @return Desired form of distance
-     */
-    public static double getDistanceToTarget(double tx, double ty, boolean getDirectDistance){
-        //if getDirectDistance is true, return the Euclidean distance from limelight to basket (straight line distance, vertical distance is included)
-        double distance;
-        if(getDirectDistance){
-            distance = HEIGHT_DIFF / Math.tan(ty) * Math.sqrt(Math.pow(Math.tan(tx),2) + Math.pow(Math.tan(ty),2) + 1);
-        }
-        else{
-            distance = getDistanceToTarget(tx,ty);
-        }
-        return distance;
-    }
-    public static double getDistanceToTarget(double tx, double ty, double cameraInclination){
-        double tyCorrected = ty + cameraInclination;
-        return getDistanceToTarget(tx, tyCorrected);
-    }
-    public static double getDistanceToTarget(double tx, double ty, double cameraInclination, boolean getDirectDistance){
-        double tyCorrected = ty + cameraInclination;
-        return getDistanceToTarget(tx, tyCorrected, getDirectDistance);
-    }
-    public static boolean checkValidVectors(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double threshold){
-        double dxCalculated = dz * (Math.tan(ty2)*Math.tan(tx1) - Math.tan(ty1)*Math.tan(tx2))/(Math.tan(ty2) - Math.tan(ty1));
+    
+    public static boolean checkValidVectors(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double yaw, double threshold){
+        double correctedTx,correctedTy1,correctedTy2;
+
+        correctedTx = tx2 + yaw;
+        correctedTy1 = ty1 + CAMERA_PITCH;
+        correctedTy2 = ty2 + CAMERA_PITCH;
+        
+        double dxCalculated = dz * (Math.tan(correctedTy2)*Math.tan(tx1) - Math.tan(correctedTy1)*Math.tan(correctedTx))/(Math.tan(correctedTy2) - Math.tan(correctedTy1));
         
         if (Math.abs(dx - dxCalculated) <= threshold){
             return true;
@@ -85,8 +64,8 @@ public class Parallax {
             return false;
         }
     }
-    public static boolean checkValidVectors(double tx1, double ty1, double tx2, double ty2, double dx, double dz){
-        return checkValidVectors(tx1,ty1,tx2,ty2,dx,dz,DEFAULT_THRESHOLD);
+    public static boolean checkValidVectors(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double yaw){
+        return checkValidVectors(tx1,ty1,tx2,ty2,dx,dz,yaw,DEFAULT_THRESHOLD);
     }
     /**
      * Returns the distance if monocular and binocular distance measures agree (within threshold), and returns -1.0 if measures don't agree.
@@ -99,9 +78,12 @@ public class Parallax {
      * @param threshold
      * @return
      */
-    public static double getAgreedDistance(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double threshold){
-        double binocularDistance = getDistanceToTarget(tx1,ty1,tx2,ty2,dx,dz);
+    public static double getAgreedDistance(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double yaw, double threshold){
+        double binocularDistance = getDistanceToTarget(tx1,ty1,tx2,ty2,dx,dz,yaw);
         double monocularDistance = getDistanceToTarget(tx2,ty2);
+        if (!checkValidVectors(tx1,ty1,tx2,ty2,dx,dz,yaw,threshold)){
+            return -1.0;
+        }
         if(Math.abs(binocularDistance - monocularDistance) <= threshold){
             return (binocularDistance + monocularDistance) / 2;
         }
@@ -109,7 +91,8 @@ public class Parallax {
             return -1.0;
         }
     }
-    public static double getAgreedDistance(double tx1, double ty1, double tx2, double ty2, double dx, double dz){
-        return getAgreedDistance(tx1,ty1,tx2,ty2,dx,dz,DEFAULT_THRESHOLD);
+    public static double getAgreedDistance(double tx1, double ty1, double tx2, double ty2, double dx, double dz, double yaw){
+        return getAgreedDistance(tx1,ty1,tx2,ty2,dx,dz,yaw,DEFAULT_THRESHOLD);
     }
+    
 }
