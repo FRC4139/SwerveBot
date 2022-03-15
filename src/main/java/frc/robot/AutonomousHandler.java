10a422e2741142e1aee78a82147d821e3ebad2a7
Saatvik-Aggarwal <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2022-03-14 23:42:22 -0700

    fixed starting stage

--- src/main/java/frc/robot/AutonomousHandler.java
+++ src/main/java/frc/robot/AutonomousHandler.java
@@ -13,7 +13,7 @@ public class AutonomousHandler {
     private final double TARGET_DISTANCE_FIRST_BALL = 6.5; // feet 
     private final double TARGET_DISTANCE_SECOND_BALL = 9; // feet
     private final double PROPORTION_DRIVE_CONSTANT = 1 / 15; 
-    private int stage = -1; 
+    private int stage = -2; 
 
     // STAGES
     // 0: back up out of tarmac until distance from target is reached
